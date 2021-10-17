40b40bce88f74146e58700ac9294e44fe359aa30
billzen <bzenelis@gmail.com>
2021-10-17 11:54:36 +0300

    Update Example.cs
    delete content

--- ConsoleApp1/Example.cs
+++ ConsoleApp1/Example.cs
@@ -5,23 +5,10 @@ using System.Threading.Tasks;
 public class Example
 {
     public static void Main()
-    {
-        Task[] tasks = new Task[2];
-        String[] files = null;
-        String[] dirs = null;
-        String docsDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+    { 
+        
 
-        tasks[0] = Task.Factory.StartNew(() => files = Directory.GetFiles(docsDirectory));
-        tasks[1] = Task.Factory.StartNew(() => dirs = Directory.GetDirectories(docsDirectory));
 
-        Task.Factory.ContinueWhenAll(tasks, completedTasks => {
-            Console.WriteLine("{0} contains: ", docsDirectory);
-            Console.WriteLine("   {0} subdirectories", dirs.Length);
-            Console.WriteLine("   {0} files", files.Length);
-        });
-       // from server 2 message
-        // 3 message from server
-       // new change from new Branch 2
         Console.ReadLine();
     }
 }

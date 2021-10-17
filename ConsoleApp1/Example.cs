e228b7c7ee5231baeb6a25249c3266d42688f565
billzen <bzenelis@gmail.com>
2021-10-17 13:10:00 +0300

    Update Example.cs
    TestNewBranch2

--- ConsoleApp1/Example.cs
+++ ConsoleApp1/Example.cs
@@ -6,30 +6,17 @@ public class Example
 {
     public static void Main()
     {
-        Task[] tasks = new Task[2];
-        String[] files = null;
-        String[] dirs = null;
-        String docsDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-        tasks[0] = Task.Factory.StartNew(() => files = Directory.GetFiles(docsDirectory));
-        tasks[1] = Task.Factory.StartNew(() => dirs = Directory.GetDirectories(docsDirectory));
-
-        Task.Factory.ContinueWhenAll(tasks, completedTasks => {
-            Console.WriteLine("{0} contains: ", docsDirectory);
-            Console.WriteLine("   {0} subdirectories", dirs.Length);
-            Console.WriteLine("   {0} files", files.Length);
-        });
-
-        //1. Local change 11/09/2021 10:28
-        //2. Server change 11/09/2021 10:32
-        //3. Local change 11/09/2021  10:35
-        //4. Local change 11/09/2021  10:45 frpm master2
-        //5. Local change 11/09/2021  11:16 from master
-
         Console.ReadLine();
     }
+    
+    public void Test1NewBranch2()
+    {
+     Console.ReadLine();
+    }
+    
+    public void Test2NewBranch2()
+    {
+     Console.ReadLine();
+    }
 }
-// The example displays output like the following:
-//       C:\Users\<username>\Documents contains:
-//          24 subdirectories
-//          16 files
+
